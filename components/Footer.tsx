9f4e22cf34fbbd7e91c1c1a5c9a2a56073fe6b49
etherealfeel <graciagodsyndrome@gmail.com>
2024-02-26 21:16:08 +0200

    feat: add footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer className="sm:px-16 py-4 px-10 flex justify-between items-center gap-2 flex-wrap rounded-lg bg-[#0f0c12]">
+            <div className="flex items-center gap-5">
+                <p className="text-base font-bold text-gray-300">
+                    @2024 Shikimori Scope
+                </p>
+                <Image
+                    src="assets/icons/logo.svg"
+                    alt="logo"
+                    width={40}
+                    height={40}
+                    className="object-contain"
+                />
+            </div>
+
+            <div className="flex items-center gap-6">
+                <Link href="https://www.tiktok.com/" target="_blank">
+                    <Image
+                        src="assets/icons/tiktok.svg"
+                        alt="logo"
+                        width={20}
+                        height={20}
+                        className="object-contain"
+                    />
+                </Link>
+
+                <Link href="https://www.instagram.com/" target="_blank">
+                    <Image
+                        src="assets/icons/instagram.svg"
+                        alt="logo"
+                        width={20}
+                        height={20}
+                        className="object-contain"
+                    />
+                </Link>
+                <Link href="https://www.twitter.com/" target="_blank">
+                    <Image
+                        src="assets/icons/twitter.svg"
+                        alt="logo"
+                        width={20}
+                        height={20}
+                        className="object-contain"
+                    />
+                </Link>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
